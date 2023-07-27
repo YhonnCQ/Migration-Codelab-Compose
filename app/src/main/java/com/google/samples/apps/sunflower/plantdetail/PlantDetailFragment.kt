6b2856dad5f950b0b249c8aa5af7493c110eba0e
Yhonn Condori <yhonncq@gmail.com>
2023-07-27 18:45:05 -0500

    viewmodels and livedata

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -75,7 +75,7 @@ class PlantDetailFragment : Fragment() {
 
             composeView.setContent {
                 MaterialTheme {
-                    PlantDetailDescription()
+                    PlantDetailDescription(plantDetailViewModel)
                 }
             }
 
